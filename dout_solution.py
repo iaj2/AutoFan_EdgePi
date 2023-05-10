6b313dddde257f6ccb631977e3e0c9a8077476f0
iaj2 <isaacjames124@yahoo.ca>
2023-05-10 22:06:43 +0100

    added dout solution

--- dout_solution.py
+++ dout_solution.py
@@ -1,15 +1,12 @@
 import time
+from edgepi.gpio.gpio_constants import GpioPins
+from edgepi.digital_output.edgepi_digital_output import EdgePiDigitalOutput
 from edgepi.tc.edgepi_tc import EdgePiTC
 from edgepi.tc.tc_constants import ConvMode
-from edgepi.dac.dac_constants import DACChannel as Ch
-from edgepi.dac.edgepi_dac import EdgePiDAC
 
-# Initialize new DAC and Thermocouple
-edgepi_dac = EdgePiDAC()
+# Initialize new Thermocouple and DOUT
 edgepi_tc = EdgePiTC()
-
-# setting DAC range 0-10V
-edgepi_dac.set_dac_gain(True)
+edgepi_dout = EdgePiDigitalOutput()
 
 # set thermocouple to measure temperature continuously
 edgepi_tc.set_config(conversion_mode=ConvMode.AUTO)
@@ -27,7 +24,6 @@ run = True
 fan_on = False
 
 while run:
-
   # wait 1 second between samples
   time.sleep(1)             
   # read cold-junction and linearized thermocouple temperatures
@@ -39,17 +35,17 @@ while run:
   if room_temp > max_temp and not fan_on:
     fan_on = True
     print("TOO HOT! Time to cool down!")
-    # write voltage value of 10 V to analog out pin number 4
-    edgepi_dac.write_voltage(Ch.AOUT4, 10)
-    # read state of DAC output 1
-    code, voltage, gain = edgepi_dac.get_state(Ch.AOUT4, True, True, True)
-    print(code,voltage,gain)
+    # setting GpioPin 4 to output direction
+    edgepi_dout.digital_output_direction(GpioPins.DOUT4, False)
+    # setting GpioPin 4 to High/On
+    edgepi_dout.digital_output_state(GpioPins.DOUT4, True)
   
   # turn off fan if room temp is below finish condition
   if room_temp < finish_temp and fan_on:
     fan_on = False
     print("Finished Cooling!")
-    edgepi_dac.reset()
+    # setting GpioPin 4 to Low/Off
+    edgepi_dout.digital_output_state(GpioPins.DOUT4, False)
 
 
 # stop continuous measurements once you're done sampling
